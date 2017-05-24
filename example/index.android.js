11aa182e6573958b0420e83e345d8697dff22274
Anoop Singh <anoop100singh@gmail.com>
2017-05-24 11:45:33 +0530

    Added example for android

--- example/index.android.js
+++ example/index.android.js
@@ -11,22 +11,159 @@ import {
   Text,
   View
 } from 'react-native';
+import AwesomeHierarchyGraph from 'react-native-d3-tree-graph'
+
+var root = {
+    name: "",
+    id: 1,
+    hidden: true,
+    children: [ {
+            name: "Q",
+            id: 16,
+            no_parent: true
+        },
+        {
+        name: "",
+        id: 2,
+        no_parent: true,
+        hidden: true,
+        children: [{
+            name: "J",
+            id: 12,
+
+        }, {
+            name: "L",
+            id: 13,
+            no_parent: true
+        }, {
+            name: "C",
+            id: 3
+        }, {
+            name: "",
+            id: 4,
+            hidden: true,
+            no_parent: true,
+            children: [{
+                name: "D",
+                id: 5,
+            }, {
+                name: "",
+                id: 14,
+                hidden: true,
+                no_parent: true,
+                children: [{
+                    name: "P",
+                    id: 15,
+                }]
+            }, {
+                name: "E",
+                id: 6,
+            }]
+        }, {
+            name: "K",
+            id: 11
+        }, {
+            name: "G",
+            id: 7,
+            children: [{
+                name: "H",
+                id: 8,
+            }, {
+                name: "I",
+                id: 9,
+            }]
+        }]
+    }, {
+        name: "M",
+        id: 10,
+        no_parent: true,
+        children: [
+          
+        ]
+    },
+    {
+        name: "anoop",
+        id: 155,
+        no_parent: true,
+        children: [{
+            name: "H",
+            id: 8,
+        }, {
+            name: "I",
+            id: 9,
+        },
+        {
+            name: "I",
+            id: 9,
+        },
+        {
+            name: "I",
+            id: 9,
+        },
+        {
+            name: "I",
+            id: 9,
+        },
+        
+      ]
+    },
+    {
+            name: "x",
+            id: 16,
+            no_parent: true
+        }
+  
+  ]
+}
+
+var siblings = [{
+    source: {
+        id: 3,
+        name: "C"
+    },
+    target: {
+        id: 11,
+        name: "K"
+    }
+}, {
+    source: {
+        id: 12,
+        name: "L"
+    },
+    target: {
+        id: 13,
+        name: "J"
+    }
+}, {
+    source: {
+        id: 5,
+        name: "D"
+    },
+    target: {
+        id: 6,
+        name: "E"
+    }
+}, {
+    source: {
+        id: 16,
+        name: "Q"
+    },
+    target: {
+        id: 10,
+        name: "M"
+    }
+}];
+
 
 export default class example extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <View style={styles.container}>
+        <AwesomeHierarchyGraph
+         root = {root}
+         siblings = {siblings}
+        />
+       </View>
     );
   }
 }
